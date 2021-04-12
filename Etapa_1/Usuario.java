2eb6478dc1f6ca5787b6db11dff6213f7ab857d7
Danniel Henrique Correa Vieira <>
2021-04-12 20:50:08 +0000

    Created updated in CRUD

--- Etapa_1/Usuario.java
+++ Etapa_1/Usuario.java
@@ -5,6 +5,9 @@ public class Usuario implements Registro {
     private String nome;
     private String email;
     private int senha;
+
+    private String perguntaSecreta;
+    private String respostaSecreta;
     
     @Override
     public int getId() {
@@ -25,6 +28,8 @@ public class Usuario implements Registro {
         dos.writeUTF(nome);
         dos.writeUTF(email);
         dos.writeInt(senha);
+        dos.writeUTF(perguntaSecreta);
+        dos.writeUTF(respostaSecreta);
 
         return baos.toByteArray();
     }
@@ -38,6 +43,8 @@ public class Usuario implements Registro {
         this.nome = dis.readUTF();
         this.email = dis.readUTF();
         this.senha = dis.readInt();
+        this.perguntaSecreta = dis.readUTF();
+        this.respostaSecreta = dis.readUTF();
     }
 
     public String getNome() {
@@ -64,23 +71,44 @@ public class Usuario implements Registro {
         this.senha = senha;
     }
 
+    public String getPerguntaSecreta() {
+        return perguntaSecreta;
+    }
+
+    public void setPerguntaSecreta(String perguntaSecreta) {
+        this.perguntaSecreta = perguntaSecreta;
+    }
+
+    public String getRespostaSecreta() {
+        return respostaSecreta;
+    }
+
+    public void setRespostaSecreta(String respostaSecreta) {
+        this.respostaSecreta = respostaSecreta;
+    }
+
     public Usuario(){
         this.id = -1;
         this.nome = "";
         this.email = "";
         this.senha = 0;
+        this.perguntaSecreta = "";
+        this.respostaSecreta = "";
     }
 
-    public Usuario(int id, String nome, String email, int senha) {
+    public Usuario(int id, String nome, String email, int senha, String perguntaSecreta, String respostaSecreta) {
         this.id = id;
         this.nome = nome;
         this.email = email;
         this.senha = senha;
+        this.perguntaSecreta = perguntaSecreta;
+        this.respostaSecreta = respostaSecreta;
     }
 
     @Override
     public String toString() {
-        return "Usuario [id=" + id + ", nome=" + nome + ", email=" + email + ", senha=" + senha + "]";
+        return "Usuario [id=" + id + ", nome=" + nome + ", email=" + email + ", senha=" + 
+        senha + ", perguntaSecreta=" + perguntaSecreta + ", respostaSecreta="+ respostaSecreta +"]";
     }
 
     
